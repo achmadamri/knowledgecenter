fd0b2c1f9836280151ad658a59dd76fd22231503
Achmad Amri <achmad.amri@gmail.com>
2023-11-30 19:52:25 +0700

    Update

--- main_api.py
+++ main_api.py
@@ -1,9 +1,7 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from typing import Optional
-
-# Your existing code goes here
 import os
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
@@ -12,19 +10,97 @@ from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from htmlTemplates import css, bot_template, user_template
-from langchain.llms import HuggingFaceHub
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-class UserQuestion(BaseModel):
-    question: str
+pdf_directory = "pdf"
 
-@app.post("/ask")
-def ask_question(user_question: UserQuestion):
-    # Your existing Streamlit logic goes here
-    response = handle_userinput(user_question.question)
-    return response
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+
+async def handle_userinput(user_question):
+    response = await app.state.conversation({'question': user_question})
+    app.state.chat_history = response['chat_history']
+
+    messages = []
+    for i, message in enumerate(app.state.chat_history):
+        if i % 2 == 0:
+            messages.append(user_template.replace("{{MSG}}", message.content))
+        else:
+            messages.append(bot_template.replace("{{MSG}}", message.content))
+    return messages
+
+
+@app.on_event("startup")
+async def startup_event():
+    load_dotenv()
+    app.state.conversation = None
+    app.state.chat_history = None
+
+    raw_text = ""
+    for pdf_filename in os.listdir(pdf_directory):
+        if pdf_filename.endswith(".pdf"):
+            pdf_path = os.path.join(pdf_directory, pdf_filename)
+            with open(pdf_path, "rb") as pdf_file:
+                pdf_reader = PdfReader(pdf_file)
+                for page in pdf_reader.pages:
+                    raw_text += page.extract_text()
+
+    text_chunks = get_text_chunks(raw_text)
+    vectorstore = get_vectorstore(text_chunks)
+    app.state.conversation = get_conversation_chain(vectorstore)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_main(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/ask_question/{user_question}", response_class=HTMLResponse)
+async def ask_question(user_question: str):
+    messages = await handle_userinput(user_question)
+    html_response = "".join(messages)
+    return HTMLResponse(content=html_response, status_code=200)
+
 
 if __name__ == "__main__":
+    import uvicorn
+
     uvicorn.run(app, host="127.0.0.1", port=8000)
